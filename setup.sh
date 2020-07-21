0d8e034d00fc1aae240d2a33f6a212de51b8b942
Rohan Joshi <rohan.joshi_@rohans-air.lan>
2020-07-21 11:54:34 -0700

    Database_URL

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ export AUTH0_DOMAIN='rohanjoshi03.us.auth0.com'
 export AUTH0_CLIENT_ID='lp7nVfQysF3Q2FSta2R4G7ZfA5HarJHE'
 export API_AUDIENCE='Casting'
 export ALGORITHMS=['RS256']
-export DATABASE_URL='postgresql://localhost/casting'
+export DATABASE_URL='postgres://wrqculteeaopzs:9a9f353a3f9314db94d2d19c4ad0be3449d7c3e2cd4472950e0ecb21ef5306c5@ec2-35-175-155-248.compute-1.amazonaws.com:5432/dllvcq1sqrqno'
